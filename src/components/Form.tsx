5aa193f41d6e3d68270be61f1f68afe9e4a3dcca
Abdullah Sarfraz <abdullahsarfraz8580@gmail.com>
2024-05-07 09:50:05 +0500

    2nd Com

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -16,11 +16,11 @@ function Form() {
   const handlePassword = (event: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(event.target.value);
   };
+  const router = useRouter();
   const handleSubmit = () => {
     if (email === "" || password === "") {
       setError("Email and password are required!");
     } else {
-      const router = useRouter();
       router.push("/Dashboard");
     }
   };
@@ -29,8 +29,8 @@ function Form() {
     <div>
       <form className="flex flex-col items-center justify-center h-96 w-96 bg-gradient-to-tr from-slate-400 via-green-200 to-pink-400 border-2 rounded-xl text-left">
         <Input
-          type="email"
           placeholder="john@doe.com"
+          type="email"
           fieldName="Email"
           value={email}
           onChange={handleEmail}
@@ -42,7 +42,7 @@ function Form() {
           value={password}
           onChange={handlePassword}
         />
-        {error && <p className="error-message">{error}</p>}
+        {error}
         <Button text="Login" onClick={handleSubmit} />
         <p className="mt-5">
           Fogot Password?{" "}
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,7 +8,9 @@ type PropsType = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-function Input({ placeholder, type, fieldName, value }: PropsType) {
+function Input({ placeholder, type, fieldName, value, onChange }: PropsType) {
+  console.log(placeholder, type, fieldName, value, onChange);
+
   return (
     <div>
       <label className="text-gray-700 font-bold text-lg">{fieldName}:</label>
@@ -16,6 +18,8 @@ function Input({ placeholder, type, fieldName, value }: PropsType) {
         className="sm:flex items-center w-72 text-left space-x-3 px-4 h-12 bg-white ring-1 ring-slate-900/10 hover:ring-slate-300 focus:outline-none focus:ring-2 focus:ring-sky-500 shadow-sm rounded-lg text-slate-400 mb-8"
         placeholder={placeholder}
         type={type}
+        value={value}
+        onChange={onChange}
         required
       ></input>
     </div>
